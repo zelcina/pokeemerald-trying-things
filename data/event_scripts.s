de16a2869607115bef0167cd20d6d93da5b75def
Hikariden <64024366+zelcina@users.noreply.github.com>
2026-02-02 04:51:58 -0500

    johto intro maps for funsies

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1741,3 +1741,7 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
+
+	.include "data/maps/NewBarkTownHouse1/scripts.inc"
+
+	.include "data/maps/NewBarkTownHouse2/scripts.inc"
